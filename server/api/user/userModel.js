0839c97630c8c453763d478ac9fe7c072fa05e5d
Danya D. Smith <danyadsmith@gmail.com>
2017-05-11 20:26:25 -0400

    Complete Exercise 8

--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -2,7 +2,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-
+  'username': {
+    type: String,
+    unique: true,
+    required: true
+  },
+  'email': {
+    type: String
+  },
+  'address': {
+    address1: String,
+    address2: String,
+    city: String,
+    state: String,
+    zip: String
+  }
 });
 
 module.exports = mongoose.model('user', UserSchema);
